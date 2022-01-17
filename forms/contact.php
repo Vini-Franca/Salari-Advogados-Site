f599a8fb5cdcd488598b7e2d490531268446a853
Vini-Franca <vinicius_santana_rf@hotmail.com>
2022-01-17 14:56:46 -0300

    site Salari Advogados

---
+++ forms/contact.php
@@ -0,0 +1,35 @@
+<?php
+
+  // Substitua contact@example.com com o email para recepcionar o email dos clientes.
+  $receiving_email_address = 'contact@example.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  // Retire o // caso queira  usar SMTP Para enviar emails.Você vai precisar entrar com a sua credencial SMTP
+  /*
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  */
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+?>
